b522227215cb9a2b640605af81b27242ad59dd11
Herbert Sauro <hsauro@u.washington.edu>
2016-08-08 11:34:57 -0700

    Fix some typos

--- fitModelToData.py
+++ fitModelToData.py
@@ -1,11 +1,6 @@
-import pylab as plt; 
-import numpy as np
-import lmfit;
-import tellurium as te; 
-import roadrunner
-import time; 
-import copy; 
-import random
+import pylab as plt; import numpy as np
+import lmfit; import tellurium as te; import roadrunner
+import time; import copy; import random
 
 #numpy.random.seed (int (time.time()))
 np.random.seed (2)
@@ -24,8 +19,10 @@ nDataPoints = 24
 m = r.simulate (0, 20, nDataPoints)
 
 x_data = m[:,0]; y_data = m[:,2]
+y_weight = np.empty([nDataPoints])
 for i in range (0, len (y_data)):
-    y_data[i] = y_data[i] + np.random.normal (0, 0.05); # Add noise
+    y_weight[i] = 0.05
+    y_data[i] = y_data[i] + np.random.normal (0, y_weight[i]); # Add noise
 
 # Compute the simulation at the current parameter values
 def my_ls_func(p):
@@ -38,17 +35,18 @@ def my_ls_func(p):
     return m[:,2]
 
 # Compute the residuals between objective and experimental data
-def f_residual(p):
-    # Ideally we should accunt for the experimental errors
-    # However the lmfit docs are not clear    
-    return y_data - my_ls_func (p)
+def weightedResiduals(p):
+    return (y_data - my_ls_func (p))/y_weight
    
+def unWeightedResiduals(p):
+    return y_data - my_ls_func (p)
+
 # Set up the parameters that we will fit
 params = lmfit.Parameters()
 params.add('k1', value=10, min=0, max=10)
 params.add('k2', value=10, min=0, max=10)
 
-minimizer = lmfit.Minimizer(f_residual, params)
+minimizer = lmfit.Minimizer(weightedResiduals, params)
 result = minimizer.minimize(method='leastsqr')
 lmfit.report_fit(result.params, min_correl=0.5)
 
@@ -58,34 +56,43 @@ for i in range(0, nParameters):
    r.model[toFit[i]] = result.params[toFit[i]].value
 m = r.simulate (0, 20, 100)
 
+# Set up some convenient font sizes
+plt.rcParams.update({'axes.titlesize': 16})
+plt.rcParams.update({'axes.labelsize': 14})
+plt.rcParams.update({'xtick.labelsize': 13})
+plt.rcParams.update({'ytick.labelsize': 13})
+
+#legend.fontsize
+
 plt.figure (figsize=(7,5))
 # Plot experimental data
-ldata, = plt.plot (x_data, y_data, 'dk', markersize=10)
+ldata, = plt.plot (x_data, y_data, 'dm', markersize=8)
 # Plot the fitted lines for S1, S2 and S3
 # Retrive lfit to use in the legend
-lfit, = plt.plot (m[:,0], m[:,1], '-k')
-plt.plot (m[:,0], m[:,2], '-k')
-plt.plot (m[:,0], m[:,3], '-k')
+lfit, = plt.plot (m[:,0], m[:,1], '-g', linewidth=2)
+plt.plot (m[:,0], m[:,2], '-g', linewidth=2)
+plt.plot (m[:,0], m[:,3], '-g', linewidth=2)
 
 # Plot the residuals
-resids = f_residual (result.params)
-lresids, = plt.plot (x_data, resids, 'ko', markersize=6)
-plt.vlines(x_data, [0], resids, color='k', linewidth=2)
+resids = unWeightedResiduals(result.params)
+lresids, = plt.plot (x_data, resids, 'bo', markersize=6)
+plt.vlines(x_data, [0], resids, color='r', linewidth=2)
 
 theResiduals = copy.deepcopy (resids)
 finalFittedData = copy.deepcopy (y_data)
 originalYData = copy.deepcopy (y_data)
 
 plt.tick_params(axis='both', which='major', labelsize=16)
-plt.xlabel('Time', fontsize=16)
+plt.xlabel('Time')
 plt.ylabel("Concentration", fontsize=16)
-plt.legend([ldata, lfit, lresids],['data', 'fit', 'residuals'], loc=0)
+plt.legend([ldata, lfit, lresids],['Data', 'Best fit', 'Residuals'], loc=0, fontsize=10)
 plt.axhline (y=0, color='k')
-plt.savefig('c:\\tmp\\foo.pdf')
+plt.savefig('fittedCurves.pdf')
 plt.show()
  
-k1Sample = []; k2Sample = []; NSamples = 1000
+k1Sample = []; k2Sample = []; NSamples = 5000
 
+chis = []
 # Start the Monte Carlo parameter confidence estimation
 for n in range (NSamples): 
    for i in range (len (finalFittedData)):
@@ -93,33 +100,47 @@ for n in range (NSamples):
 
    result = minimizer.minimize(method='leastsqr')
    # Not all fits will work so we test against a threshold   
-   if result.redchi < 0.01: 
+   if result.redchi < 4: 
+      chis.append (result.redchi)
       pp = result.params.valuesdict()
       # Collect the fitted parameters
       k1Sample.append (pp['k1'])
       k2Sample.append (pp['k2'])
    
-plt.xlabel('k1', fontsize=16); plt.ylabel('k2', fontsize=16)
+plt.xlabel('k1'); plt.ylabel('k2')
 plt.plot (k1Sample, k2Sample, '.b')
 
+# Compute the mean values of the k1 and k2 samples
 meank1 = np.mean (k1Sample); meank2 = np.mean (k2Sample)
+
 # Compute 95% percentiles
 plusk1 = np.percentile (k1Sample, 97.5) - meank1
-minusk1 = meank1 - np.percentile (k1Sample, 5)
+minusk1 = meank1 - np.percentile (k1Sample, 2.5)
 
 plusk2 = np.percentile (k2Sample, 97.5) - meank2
-minusk2 = meank2 - np.percentile (k2Sample, 5)
+minusk2 = meank2 - np.percentile (k2Sample, 2.5)
 
 # Note that the limits are not symmetric
 print 'Computed 95 percent percentiles'
-print meank1, "+/- ", plusk1, minusk1
-print meank2, "+/- ", plusk2, minusk2
+print 'k1: ', meank1, "+/- ", plusk1, minusk1
+print 'k2: ', meank2, "+/- ", plusk2, minusk2
 
 plt.hist (k1Sample, 12)
-plt.ylabel('Frequency'); plt.xlabel ('k1'); plt.title ('k1 variation')
+plt.ylabel('Frequency'); 
+plt.xlabel ('k1'); plt.title ('k1 variation')
+plt.savefig('k1Distribution.pdf')
 plt.show()
 plt.hist (k2Sample, 12)
-plt.ylabel('Frequency'); plt.xlabel ('k2'); plt.title ('k2 variation');
+plt.ylabel('Frequency'); 
+plt.xlabel ('k2'); plt.title ('k2 variation');
+plt.savefig('k2Distribution.pdf')
 plt.show()
 
+# Scatter plot of k1 versus k2
+plt.xlim((0.1, 0.6)); plt.ylim ((0.1, 0.5))
+plt.plot (k1Sample, k2Sample, '.')
+plt.xlabel ('k1'); plt.ylabel ('k2')
+plt.title ('Scatter plot of k1 against k2')
+plt.savefig('k1_k2_scatter.pdf')
+plt.show()
 
