c2271f523d32c56e7851ea78ae097939a2cab11a
Herbert Sauro <hsauro@u.washington.edu>
2016-08-09 11:19:30 -0700

    Updated symbols

--- parameterScanBistableSystem.py
+++ parameterScanBistableSystem.py
@@ -1,20 +1,22 @@
- Time course parameter scan of a bistable system
+# Time course parameter scan of a bistable system
 import tellurium as te
 import pylab as plt; 
 
 r = te.loada('''
-    J1: $Xo -> x; 0.1 + k1*x^4/(k2+x^4);
-    x -> $w; k3*x;
+    J1: $Xo -> S1; 0.1 + k1*S1^4/(k2+S1^4);
+    S1 ->; k3*S1;
     k1 = 0.9;
     k2 = 0.3;
     k3 = 0.7;
-    x = 0;
+    S1 = 0;
 ''')
 
-result = r.simulate (0, 12, 100, ['time', 'x'])
+S1Ref = 0.1
+#result = r.simulate (0, 12, 100, ['time', 'S1'])
 for x in plt.arange (0.1, 2, 0.2): 
-    r.x = x
-    m = r.simulate (0, 12, 100, ['x'], linewidth=2)
-    plt.plot (result[:,0], m[:,0])    
+    m = r.simulate (0, 12, 100, ['Time', 'S1'])
+    plt.plot (m[:,0], m[:,1], linewidth=2)  
+    S1Ref = S1Ref + 0.1
+    r.S1 = S1Ref
     
 plt.show()
